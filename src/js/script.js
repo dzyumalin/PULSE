5ac0413fd034edcd7f25855cf71631a3aa61ce81
Dima <zyumalin@bk.ru>
2020-02-28 14:03:03 +0300

    section carousel (slick-slide)r

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1500,
+        adaptiveHeight: false,
+        prevArrow: '<button type="button" class="prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                dots: true,
+                arrows: false
+              }
+            }
+        ]
+    });
+});
