f544d969636ea11dcd799ab0a72f490b35130c69
Dima <zyumalin@bk.ru>
2020-03-14 20:15:32 +0300

    animation blocks: advantages, catalog and feed. layout validation

--- src/js/script.js
+++ src/js/script.js
@@ -116,9 +116,13 @@ function validateForms(form){
         }
     });
 
-    $("a[href^='#']").click(function(){
+    $("a[href=#up]").click(function(){
         const _href = $(this).attr("href");
         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
         return false;
     });
+
+    // fadein animation
+
+    new WOW().init();
 });
